72a45f143490509c6e605b7fa569c1c4a9ed65e8
DanLDB <dpbergquist@gmail.com>
2015-09-25 11:34:16 +0100

    count total todos

--- test/e2e/todosFeature.js
+++ test/e2e/todosFeature.js
@@ -1,19 +1,29 @@
 describe('Todos list', function() {
+  var todoList = element.all(by.repeater('todo in todoList.todos'));
+
+  beforeEach(function(){
+    browser.get('http://localhost:8000');
+  });
+
   it('has a title', function() {
     browser.get('http://localhost:8000');
     expect(browser.getTitle()).toEqual('Stuff Todo');
   });
 
   it('accepts an input', function(){
-    var todoList = element.all(by.repeater('todo in todoList.todos'));
-    element(by.model('todoList.todoText')).sendKeys('Do all the things!');
-    element(by.css('[id=submitTodo]')).click();
+    inputTodo();
     expect(todoList.count()).toEqual(1);
-    expect(todoList.last().getText()).toEqual('Do all the things!')
+    expect(todoList.last().getText()).toEqual('Do all the things!');
   })
 
   it('shows a total number of todos', function() {
     var todosCount = element(by.css('[id=todoCount]'));
-    expect(todosCount)
-  })
+    inputTodo();
+    expect(todosCount.getText()).toEqual('1');
+  });
+
+  function inputTodo () {
+    element(by.model('todoList.todoText')).sendKeys('Do all the things!');
+    element(by.css('[id=submitTodo]')).click();
+  };
 });
